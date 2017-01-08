06663302d37ba4d5c69741d83b4ae3938cbe22e9
Mitchel Kelonye <kelonyemitchel@gmail.com>
2017-01-08 11:16:47 +0300

    Release 2.11.0

--- test/support/server/mongo.js
+++ test/support/server/mongo.js
@@ -4,6 +4,10 @@
 var mongoose = require('mongoose');
 var uri = 'mongodb://localhost/test';
 
+// promise
+
+mongoose.Promise = Promise;
+
 // expose
 
 module.exports = mongoose.createConnection(uri);
